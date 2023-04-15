afb77bd1e60e50dcac09458d8e6ebb2aec1a864f
jrbdino <117558328+jrbdino@users.noreply.github.com>
2023-04-15 15:11:06 -0500

    Added jumping, brought player objects in from characters.py
    also refactored chicken objects to player object
    
    plus story

--- characters.py
+++ characters.py
@@ -3,6 +3,6 @@ import pygame
 
 # Chicken Graphics
 
-chick_surf = pygame.image.load('chicken_graphics/Chickmen.png').convert_alpha()
-chick_scaled = pygame.transform.scale(chick_surf, (150, 150))
-chick_rect = chick_surf.get_rect(topleft=(150, 150))
+player_surf = pygame.image.load('chicken_graphics/Chickmen.png').convert_alpha()
+player_scaled = pygame.transform.scale(player_surf, (150, 150))
+player_rect = player_surf.get_rect(topleft=(150, 150))
--- game_loop.py
+++ game_loop.py
@@ -8,8 +8,11 @@ clock = pygame.time.Clock()
 # init font if needed here
 
 # import image surface rectangle objects here
-# scuffed imports stay here, do not move out of order
-from characters import *
+# player graphics
+player_surf = pygame.image.load('chicken_graphics/Chickmen.png').convert_alpha()
+player_surf = pygame.transform.scale(player_surf, (150, 150))
+player_rect = player_surf.get_rect(bottomleft=(30, 465))
+player_gravity = 0
 
 # Background and sky
 back_sky = pygame.image.load('chicken_graphics/Environment/sky.png')
@@ -17,8 +20,7 @@ sky_scaled = pygame.transform.scale(back_sky, (1080, 620))
 back_ground = pygame.image.load('chicken_graphics/Environment/ground.png')
 ground_scaled = pygame.transform.scale(back_ground, (1080, 200))
 
-# Gregory
-chicken = pygame.image.load('chicken_graphics/Chickmen.png')
+
 
 # main loop
 while 1:
@@ -27,9 +29,20 @@ while 1:
             pygame.quit()
             exit()
 
+        if event.type == pygame.KEYDOWN and player_rect.bottom >= 465:
+            if event.key == pygame.K_SPACE:
+                player_gravity -= 20
+
     screen.blit(sky_scaled, (0, 0))
     screen.blit(ground_scaled, (0, 425))
-    screen.blit(chicken, (30, 245))
+
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 465:
+        player_rect.bottom = 465
+        player_gravity = 0
+
+    screen.blit(player_surf, player_rect)
 
     pygame.display.update()
     clock.tick(60)
