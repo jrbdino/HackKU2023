781f66f10174e1de3e379c2a97f2203577bac34f
Arianna Cabrera <121651388+ariannacabrera@users.noreply.github.com>
2023-04-15 17:48:26 -0500

    update game over

---
+++ game_over.py
@@ -0,0 +1,25 @@
+# game over image
+end_sky = pygame.image.load('chicken_graphics/Environment/end_sunset.png').convert()
+end_sky_scaled = pygame.transform.scale(end_sky, (1080, 620))
+
+# game over screen
+#if game_active:
+   # pass
+#else:  # need to play with coords, use same colors if they have enough contrast or use similar colors
+screen.blit(end_sky_scaled, (0, 0))
+
+player_rect.midbottom(100, 300)
+screen.blit(player_stand, player_stand_rect)
+# player_gravity = 0 cut if not needed
+
+over_message = test_font.render("Game Over", False, "#A93226")
+over_message_rect = over_message.get_rect(top=(540, 50))
+screen.blit(over_message, over_message_rect)
+
+score_message = test_font.render(f'You scored {score} points!', False, "#A93226")
+score_message_rect = score_message.get_rect(centerright=(540, 310))
+screen.blit(game_name, game_name_rect)
+
+tryagain_message = test_font.render("Press SPACEBAR to try again", False, "#A93226")
+tryagain_message_rect = tryagain_message.get_rect(bottom=(540, 570))
+screen.blit(tryagain_message, tryagain_message_rect)
