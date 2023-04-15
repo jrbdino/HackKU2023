ba834e6b9947552b79e5df2119f43a4cefeff7a4
DylNorr1s <dylnorrisj@gmail.com>
2023-04-15 11:50:37 -0500

    Update characters.py
    Added surface, rescaled, created rectangle

--- characters.py
+++ characters.py
@@ -1,4 +1,6 @@
 import pygame
 
 # Chicken Graphics
-chick_surface = pygame.image.load('chicken_graphics/Chickmen.png').convert_alpha()
+chick_surf = pygame.image.load('chicken_graphics/Chickmen.png').convert_alpha()
+chick_scaled = pygame.transform.scale(chick_surf, (150, 150))
+chick_rect = chick_surf.get_rect(topleft=(150, 150))
