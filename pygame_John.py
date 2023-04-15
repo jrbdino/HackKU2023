ed461de10c004eeb33c814971445fd8786cf5341
jrbdino <117558328+jrbdino@users.noreply.github.com>
2023-04-14 22:45:44 -0500

    deleted all .DS_Store files and added to .gitignore

--- pygame_John.py
+++ pygame_John.py
@@ -1,4 +1,21 @@
-import pygame as pg
+import pygame
+from sys import exit
 
-pg.init()
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Runner")
+clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(test_surface, (200,100))
+
+    pygame.display.update()
+    clock.tick(60)
