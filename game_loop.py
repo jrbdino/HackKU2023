7950d172d683ca3a222973b13e2c662c11eaa51d
DylNorr1s <dylnorrisj@gmail.com>
2023-04-15 13:43:46 -0500

    Update game_loop.py
    Added greg

--- game_loop.py
+++ game_loop.py
@@ -11,10 +11,14 @@ clock = pygame.time.Clock()
 # scuffed imports stay here, do not move out of order
 from characters import *
 
+# Background and sky
 back_sky = pygame.image.load('chicken_graphics/Environment/sky.png')
 sky_scaled = pygame.transform.scale(back_sky, (1080, 620))
 back_ground = pygame.image.load('chicken_graphics/Environment/ground.png')
 ground_scaled = pygame.transform.scale(back_ground, (1080, 200))
+
+# Gregory
+chicken = pygame.image.load('chicken_graphics/Chickmen.png')
 
 # main loop
 while 1:
@@ -25,6 +29,7 @@ while 1:
 
     screen.blit(sky_scaled, (0, 0))
     screen.blit(ground_scaled, (0, 425))
+    screen.blit(chicken, (30, 245))
 
     pygame.display.update()
     clock.tick(60)
