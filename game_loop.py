dd4290ac0fa4504d558fd37c06187f1bea7be2e8
DylNorr1s <dylnorrisj@gmail.com>
2023-04-15 22:28:37 -0500

    Went Crazy Mode
    Implemented Arianna's end screens and Aoi's graphics and titles. Need to readjust text still

--- game_loop.py
+++ game_loop.py
@@ -5,9 +5,10 @@ from random import randint
 
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
-    score_surf = test_font.render(f' {current_time}', False, (64, 64, 64))
+    score_surf = game_font.render(f' {current_time}', False, (64, 64, 64))
     score_rect = score_surf.get_rect(center=(540, 75))
     screen.blit(score_surf, score_rect)
+    return current_time
 
 
 def obstacle_movement(obstacle_list):
@@ -48,9 +49,10 @@ pygame.init()
 screen = pygame.display.set_mode((1080, 620))
 pygame.display.set_caption("Gregory's Great Game")
 clock = pygame.time.Clock()
-game_running = True
+game_running = False
 start_time = 0
-test_font = pygame.font.Font('pygame/fonts/Pixeltype.ttf', 50)
+score = 0
+game_font = pygame.font.Font('pygame/fonts/Pixeltype.ttf', 50)
 
 # import image surface rectangle objects here
 # player graphics
@@ -80,24 +82,33 @@ obstacle_rect_list = []
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, 1500)
 
+# Game title
+game_title = game_font.render("Game Game", False, "#A93226")
+game_title_rect = game_title.get_rect(midtop=(540, 310))
+
 # main loop
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if game_running:
+            if event.type == pygame.KEYDOWN and player_rect.bottom >= 465:
+                if event.key == pygame.K_SPACE:
+                    player_gravity -= 20
 
-        if event.type == pygame.KEYDOWN and player_rect.bottom >= 465:
-            if event.key == pygame.K_SPACE:
-                player_gravity -= 20
+            if event.type == obstacle_timer:
+                obstacle_rect_list.append(fox_surf.get_rect(bottomleft=(randint(1080, 1380), 465)))
 
-        if event.type == obstacle_timer:
-            obstacle_rect_list.append(fox_surf.get_rect(bottomleft=(randint(1080, 1380), 465)))
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_running = True
+                obstacle_rect_list = []
+                start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_running:
         screen.blit(sky_scaled, (0, -2))
         screen.blit(ground_scaled, (0, 425))
-
         score = display_score()
 
         player_gravity += 1
@@ -115,33 +126,53 @@ while 1:
 
         # collisions
         game_running = collisions(player_rect, obstacle_rect_list)
+    # Game over screen, text, in the else statement
+    else:
+        game_name = game_font.render("Gregory's  Great  Game", False, '#693C98')
+        game_name_rect = game_name.get_rect(center=(540, 150))
+
+
+
+        over_message = game_font.render("Game Over", False, "#A93226")
+        over_message_rect = over_message.get_rect(topleft=(540, 50))
+
+        score_message = game_font.render(f'You scored {score} points!', False, "#A93226")
+        score_message_rect = score_message.get_rect(midright=(540, 310))
+
+        tryagain_message = game_font.render("Press SPACEBAR to try again", False, "#A93226")
+        tryagain_message_rect = tryagain_message.get_rect(midbottom=(540, 570))
+
+        if score == 0:
+            start_screen = pygame.image.load('chicken_graphics/Environment/start_sunrise.png').convert_alpha()
+            start_scaled = pygame.transform.scale(start_screen, (1080, 620))
+
+            player_stand = pygame.image.load('chicken_graphics/Chickmen_jump.png').convert_alpha()
+            player_stand = pygame.transform.scale(player_stand, (200, 170))
+            player_stand_rect = player_stand.get_rect(center=(260, 280))
+
+            fox_stand = pygame.image.load('chicken_graphics/fox.png').convert_alpha()
+            fox_stand = pygame.transform.scale(fox_stand, (200, 100))
+            fox_stand_rect = fox_stand.get_rect(center=(900, 400))
+
+            game_message = game_font.render('Press  Space  to  Start', False, '#693C98')
+            game_message_rect = game_message.get_rect(center=(540, 450))
+
+            screen.blit(start_scaled, (0, 0))
+            screen.blit(game_name, game_name_rect)
+            screen.blit(player_stand, player_stand_rect)
+            screen.blit(fox_stand, fox_stand_rect)
+            screen.blit(game_message, game_message_rect)
+
+        else:
+            end_sky = pygame.image.load('chicken_graphics/Environment/end_sunset.png').convert_alpha()
+            end_sky_scaled = pygame.transform.scale(end_sky, (1080, 620))
+
+            screen.blit(end_sky_scaled, (0, 0))
+            screen.blit(score_message, score_message_rect)
+            screen.blit(tryagain_message, tryagain_message_rect)
+            screen.blit(over_message, over_message_rect)
+            player_rect = player_scaled.get_rect(midleft=(100, 300))
+            screen.blit(player_walk1, player_rect)
 
     pygame.display.update()
     clock.tick(60)
-
-
-# game over image
-end_sky = pygame.image.load('chicken_graphics/Environment/end_sunset.png').convert_alpha()
-end_sky_scaled = pygame.transform.scale(end_sky, (1080, 620))
-
-# game over screen
-#if game_active:
-   # pass
-#else:  # need to play with coords, use same colors if they have enough contrast or use similar colors
-screen.blit(end_sky_scaled, (0, 0))
-
-player_rect = player_scaled.get_rect(midleft=(100, 300))
-screen.blit(player_walk1, player_rect)
-# player_gravity = 0 cut if not needed
-
-over_message = test_font.render("Game Over", False, "#A93226")
-over_message_rect = over_message.get_rect(top=(540, 50))
-screen.blit(over_message, over_message_rect)
-
-score_message = test_font.render(f'You scored {score} points!', False, "#A93226")
-score_message_rect = score_message.get_rect(centerright=(540, 310))
-screen.blit(score_message, score_message_rect)
-
-tryagain_message = test_font.render("Press SPACEBAR to try again", False, "#A93226")
-tryagain_message_rect = tryagain_message.get_rect(bottom=(540, 570))
-screen.blit(tryagain_message, tryagain_message_rect)
